6655c98dc6190e058b6a48654dcb54a3e3df2c96
vladstetca-oss <vlad.stetca@ontariotechu.net>
2026-02-11 19:35:26 -0500

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -1,92 +1,116 @@
 package com.ontariotechu.sofe3980U;
 
-public class Binary {
-    private String value;
-    
-    public Binary(String value) {
-
-	        if (value == null || value.isEmpty()) {
-            this.value = "0";
-            return;
-        }
-        
-        StringBuilder filtered = new StringBuilder();
-        for (int i = 0; i < value.length(); i++) {
-            char c = value.charAt(i);
-            if (c == '0' || c == '1') {
-                filtered.append(c);
-            }
-        }
-        
-        if (filtered.length() == 0) {
-            this.value = "0";
-            return;
-        }
-        
-        String binaryStr = filtered.toString();
-        int firstOne = binaryStr.indexOf('1');
-        if (firstOne == -1) {
-            this.value = "0"; 
-        } else {
-            this.value = binaryStr.substring(firstOne);
-        }
-    }
-    
-    public String getValue() {
-        return value;
-    }
-    
-    private int toDecimal() {
-        int result = 0;
-        int power = 1; // 2^0
-        
-        for (int i = value.length() - 1; i >= 0; i--) {
-            if (value.charAt(i) == '1') {
-                result += power;
-            }
-            power *= 2;
-        }
-        return result;
-    }
-    
-    private static String fromDecimal(int decimal) {
-        if (decimal == 0) {
-            return "0";
-        }
-        
-        StringBuilder binary = new StringBuilder();
-        while (decimal > 0) {
-            binary.insert(0, decimal % 2);
-            decimal = decimal / 2;
-        }
-        return binary.toString();
-    }
-    
-    public Binary add(Binary other) {
-        int num1 = this.toDecimal();
-        int num2 = other.toDecimal();
-        int sum = num1 + num2;
-        return new Binary(fromDecimal(sum));
-    }
-    
-    public Binary or(Binary other) {
-        int num1 = this.toDecimal();
-        int num2 = other.toDecimal();
-        int result = num1 | num2;
-        return new Binary(fromDecimal(result));
-    }
-    
-    public Binary and(Binary other) {
-        int num1 = this.toDecimal();
-        int num2 = other.toDecimal();
-        int result = num1 & num2;
-        return new Binary(fromDecimal(result));
-    }
-    
-    public Binary multiply(Binary other) {
-        int num1 = this.toDecimal();
-        int num2 = other.toDecimal();
-        int product = num1 * num2;
-        return new Binary(fromDecimal(product));
-    }
+public class Binary
+{
+	private String number="0";
+	
+	public Binary(String number) {
+		if (number == null || number.isEmpty()) {
+			this.number = "0";
+			return;
+		}
+	
+		for (int i = 0; i < number.length(); i++) {
+			char ch = number.charAt(i);
+			if (ch != '0' && ch != '1') {
+				this.number = "0";
+				return;
+			}
+		}
+	
+		int beg;
+		for (beg = 0; beg < number.length(); beg++) {
+			if (number.charAt(beg) != '0') {
+				break;
+			}
+		}
+	
+		this.number = (beg == number.length()) ? "0" : number.substring(beg);
+	
+		if (this.number.isEmpty()) {
+			this.number = "0";
+		}
+	}
+	
+	public String getValue()
+	{
+		return this.number;
+	}
+	
+	public static Binary or(Binary num1, Binary num2)
+	{
+	    int i = num1.number.length() - 1;
+	    int j = num2.number.length() - 1;
+	    String result = "";
+	    while (i >= 0 || j >= 0)
+	    {
+	        char b1 = (i >= 0) ? num1.number.charAt(i) : '0';
+	        char b2 = (j >= 0) ? num2.number.charAt(j) : '0';
+	        result = ((b1 == '1' || b2 == '1') ? "1" : "0") + result;
+	        i--;
+	        j--;
+	    }
+	    return new Binary(result);
+	}
+	
+	public static Binary multiply(Binary num1, Binary num2)
+	{
+	    Binary result = new Binary("0");
+	    int shift = 0;
+	    for (int i = num2.number.length() - 1; i >= 0; i--)
+	    {
+	        if (num2.number.charAt(i) == '1')
+	        {
+	            String shiftedValue = num1.number;
+	            for (int k = 0; k < shift; k++)
+	            {
+	                shiftedValue += "0";
+	            }
+	            result = add(result, new Binary(shiftedValue));
+	        }
+	        shift++;
+	    }
+	    return result;
+	}
+	
+	public static Binary and(Binary num1, Binary num2)
+	{
+	    int i = num1.number.length() - 1;
+	    int j = num2.number.length() - 1;
+	    String result = "";
+	    while (i >= 0 || j >= 0)
+	    {
+	        char b1 = (i >= 0) ? num1.number.charAt(i) : '0';
+	        char b2 = (j >= 0) ? num2.number.charAt(j) : '0';
+	        result = ((b1 == '1' && b2 == '1') ? "1" : "0") + result;
+	        i--;
+	        j--;
+	    }
+	    return new Binary(result);
+	}
+	
+	public static Binary add(Binary num1,Binary num2)
+	{
+		int ind1=num1.number.length()-1;
+		int ind2=num2.number.length()-1;
+		int carry=0;
+		String num3="";
+		while(ind1>=0 ||  ind2>=0 || carry!=0)
+		{
+			int sum=carry;
+			if(ind1>=0){
+				sum += (num1.number.charAt(ind1)=='1')? 1:0;
+				ind1--;
+			}
+			if(ind2>=0){
+				sum += (num2.number.charAt(ind2)=='1')? 1:0;
+				ind2--;
+			}
+			carry=sum/2;
+			sum=sum%2;
+			num3 =( (sum==0)? "0":"1")+num3;
+		}
+		Binary result=new Binary(num3);
+		return result;
+	}
 }
